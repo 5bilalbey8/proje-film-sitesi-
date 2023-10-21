8eda0c1eb8f55f66f47f9391899af53d1e240413
5bilalbey8 <134318346+5bilalbey8@users.noreply.github.com>
2023-10-22 01:33:53 +0300

    Add files via upload

---
+++ Film Sitesi/script.js
@@ -0,0 +1,5 @@
+const ball=document.querySelector(".toggle-ball");
+    const items=document.querySelectorAll(".container,.navbar,.sidebar,.sidebar i,.toggle,.toggle-ball,.movie-list-filter select,.movie-list-title")
+    ball.addEventListener("click",function(){
+        items.forEach((item) => item.classList.toggle("active"));
+})
